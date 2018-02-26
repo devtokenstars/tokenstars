0d66dc27e4a32de6b327a9b9edb26f0bb9f0ca35
devtokenstars <dev@tokenstars.com>
2018-02-26 17:27:23 +0800

    localition files

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -39,6 +39,7 @@ return [
     'dimensions'           => 'The :attribute has invalid image dimensions.',
     'distinct'             => 'The :attribute field has a duplicate value.',
     'email'                => 'The :attribute must be a valid email address.',
+    'eth_address'          => 'The :attribute must be a valid Ethereum address.',
     'exists'               => 'The selected :attribute is invalid.',
     'file'                 => 'The :attribute must be a file.',
     'filled'               => 'The :attribute field must have a value.',
@@ -117,5 +118,4 @@ return [
     */
 
     'attributes' => [],
-
 ];
